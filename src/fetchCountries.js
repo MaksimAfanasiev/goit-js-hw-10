866083f5348b615c9fc4e887df43310512624b92
MaksimAfanasiev <m_afanasyev@ukr.net>
2022-05-24 19:16:22 +0300

    Almoust done
    Add all JS-code?, without css and refactoring

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,18 @@
+import Notiflix from 'notiflix';
+
 const requestFields = ['name', 'capital', 'population', 'flags', 'languages'];
 
-export function fetchCountries(name) {
-  const URL = `https://restcountries.com/v3.1/name/${name}?fields=${requestFields.join(',')}`;
+export function fetchCountries(request) {
+  const URL = `https://restcountries.com/v3.1/name/${request}?fields=${requestFields.join(',')}`;
 
-  fetch(URL)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
+  return fetch(URL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error();
+      } else {
+        return response.json();
+      }
+    })
+    .then(data => data)
+    .catch(error => error);
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries.js';
 import _debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const inputEl = document.getElementById('search-box');
-const searchListEl = document.querySelector('.country-list');
+const countryListEl = document.querySelector('.country-list');
 const infoDivEl = document.querySelector('.country-info');
 
 const DEBOUNCE_DELAY = 300;
@@ -13,11 +14,61 @@ inputEl.addEventListener('input', _debounce(onSearchInput, DEBOUNCE_DELAY));
 function onSearchInput(e) {
   const searchRequest = e.target.value.trim();
   if (!searchRequest) {
-    searchListEl.innerHTML = '';
+    countryListEl.innerHTML = '';
     infoDivEl.innerHTML = '';
   } else {
-    fetchCountries(searchRequest);
-    searchListEl.innerHTML = 'Some country list';
-    infoDivEl.innerHTML = 'Some info about ech country';
+    fetchCountries(searchRequest)
+      .then(data => {
+        if (data.length > 10) {
+          countryListEl.innerHTML = '';
+          infoDivEl.innerHTML = '';
+          return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        } else {
+          if (data.length >= 2) {
+            countryListEl.innerHTML = '';
+            infoDivEl.innerHTML = '';
+
+            const listMarkup = makeListMarkup(data);
+            countryListEl.insertAdjacentHTML('beforeend', listMarkup);
+          } else {
+            countryListEl.innerHTML = '';
+            infoDivEl.innerHTML = '';
+
+            const divMarkup = makeDivMarkup(data);
+            infoDivEl.insertAdjacentHTML('beforeend', divMarkup);
+          }
+        }
+      })
+      .catch(() => Notiflix.Notify.failure('Oops, there is no country with that name'));
   }
+}
+
+function makeListMarkup(array) {
+  return array
+    .map(country => {
+      const flag = country.flags.svg;
+      const name = country.name.official;
+
+      return `<li class="list-item"><img src=${flag} alt="flag of ${name}" width="40">${name}</li>`;
+    })
+    .join('');
+}
+
+function makeDivMarkup(array) {
+  const country = array[0];
+
+  const flag = country.flags.svg;
+  const name = country.name.official;
+  const capital = country.capital.join(', ');
+  const languages = Object.values(country.languages).join(', ');
+  const population = country.population;
+
+  console.log(languages);
+
+  const markup = `
+      <h1 class="country-name"><img src=${flag} alt="flag of ${name}" width="40">${name}</h1>
+      <p class="country-descr"><span class="country-index">Capital: </span>${capital}</p>
+      <p class="country-descr"><span class="country-index">Population: </span>${population}</p>
+      <p class="country-descr"><span class="country-index">Languages: </span>${languages}</p>`;
+  return markup;
 }
