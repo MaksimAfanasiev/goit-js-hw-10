537fad1aa322dc6b6d432f08eb75fa5f126e5865
MaksimAfanasiev <m_afanasyev@ukr.net>
2022-05-31 02:12:55 +0300

    start oldParcel

--- src/index.js
+++ src/index.js
@@ -1,72 +1,126 @@
-import './css/styles.css';
-import { fetchCountries } from './fetchCountries.js';
-import _debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+const axios = require('axios');
 
-const inputEl = document.getElementById('search-box');
-const countryListEl = document.querySelector('.country-list');
-const infoDivEl = document.querySelector('.country-info');
+const formEl = document.querySelector('#search-form');
+const galleryEl = document.querySelector('.gallery');
+const moreBtn = document.querySelector('.load-more');
 
-const DEBOUNCE_DELAY = 300;
+// BackEnd
 
-inputEl.addEventListener('input', _debounce(onSearchInput, DEBOUNCE_DELAY));
+// Variables for HTTP-request
 
-function onSearchInput(e) {
-  const searchRequest = e.target.value.trim();
-  if (!searchRequest) {
-    countryListEl.innerHTML = '';
-    infoDivEl.innerHTML = '';
-  } else {
-    fetchCountries(searchRequest)
-      .then(data => {
-        if (data.length > 10) {
-          countryListEl.innerHTML = '';
-          infoDivEl.innerHTML = '';
-          return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-        } else {
-          if (data.length >= 2) {
-            countryListEl.innerHTML = '';
-            infoDivEl.innerHTML = '';
+const URL = 'https://pixabay.com/api/';
+const API_KEY = '27715674-92925a6f691a5283ca5f8bc26';
+let q = '';
+const image_type = 'photo';
+const orientation = 'horizontal';
+const safesearch = 'true';
+let page = 1;
+const per_page = 40;
+let totalPages = 0;
 
-            const listMarkup = makeListMarkup(data);
-            countryListEl.insertAdjacentHTML('beforeend', listMarkup);
-          } else {
-            countryListEl.innerHTML = '';
-            infoDivEl.innerHTML = '';
+function pathMaker() {
+  return `${URL}?key=${API_KEY}&q=${q}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}&page=${page}&per_page=${per_page}`;
+}
 
-            const divMarkup = makeDivMarkup(data);
-            infoDivEl.insertAdjacentHTML('beforeend', divMarkup);
-          }
-        }
-      })
-      .catch(() => Notiflix.Notify.failure('Oops, there is no country with that name'));
+// const PATH = `${URL}?key=${API_KEY}&q=${q}&image_type=${image_type}&orientation=${orientation}&safesearch=${safesearch}&page=${page}&per_page=${per_page}`;
+
+// -------------------------------
+
+formEl.addEventListener('submit', onSubmitForm);
+moreBtn.addEventListener('click', onMoreBtnClick);
+
+console.log('hello');
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  page = 1;
+  q = e.target.searchQuery.value;
+  galleryEl.innerHTML = '';
+
+  const PATH = !q.trim() ? '' : pathMaker();
+
+  request(PATH)
+    .then(data => {
+      console.log(data);
+      totalPages = data.totalHits;
+      makeMarkupGallery(data.hits);
+    })
+    .catch(error => console.log(error));
+}
+
+function onMoreBtnClick() {
+  page += 1;
+  const PATH = !q.trim() ? '' : pathMaker(q, page);
+
+  request(PATH)
+    .then(data => {
+      makeMarkupGallery(data.hits);
+    })
+    .catch(error => console.log(error));
+
+  if (page === totalPages) {
+    console.log('Hide a button Load_more');
+    alert("We're sorry, but you've reached the end of search results.");
   }
 }
 
-function makeListMarkup(array) {
-  return array
-    .map(country => {
-      const flag = country.flags.svg;
-      const name = country.name.official;
+// Request
 
-      return `<li class="list-item"><img src=${flag} alt="flag of ${name}" width="40" height="40">${name}</li>`;
-    })
-    .join('');
+function request(url) {
+  if (!url) {
+    alert('Search field can not be empthy!');
+  } else {
+    return axios
+      .get(url)
+      .then(response => {
+        if (response.data.hits.length === 0) {
+          alert(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+          throw new Error();
+        } else {
+          return response.data;
+        }
+      })
+      .catch(error => error);
+  }
 }
 
-function makeDivMarkup(array) {
-  const country = array[0];
+// Markup
 
-  const flag = country.flags.svg;
-  const name = country.name.official;
-  const capital = country.capital.join(', ');
-  const languages = Object.values(country.languages).join(', ');
-  const population = country.population;
+function makeMarkupGallery(array) {
+  const galleryMarkup = array
+    .map(img => {
+      const smallImg = img.webformatURL;
+      const largeImg = img.largeImageURL;
+      const alt = img.tags;
+      const likes = img.likes;
+      const views = img.views;
+      const comments = img.comments;
+      const downloads = img.downloads;
 
-  const markup = `
-      <h1 class="country-name"><img src=${flag} alt="flag of ${name}" width="60" height="60">${name}</h1>
-      <p class="country-descr"><span class="country-index">Capital: </span>${capital}</p>
-      <p class="country-descr"><span class="country-index">Population: </span>${population}</p>
-      <p class="country-descr"><span class="country-index">Languages: </span>${languages}</p>`;
-  return markup;
+      return `
+        <div class="photo-card">
+        <img src=${smallImg} alt=${alt} loading="lazy" />
+        <div class="info">
+            <p class="info-item">
+            <b>Likes ${likes}</b>
+            </p>
+            <p class="info-item">
+            <b>Views ${views}</b>
+            </p>
+            <p class="info-item">
+            <b>Comments ${comments}</b>
+            </p>
+            <p class="info-item">
+            <b>Downloads ${downloads}</b>
+            </p>
+        </div>
+        </div>`;
+    })
+    .join(',');
+
+  galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
 }
